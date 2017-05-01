21846b73197d03bb5f0792a5a3ea32ba4a91050e
Byung-Chun Yoo <byung-chunyoo@zipcoder7.local>
2017-05-01 15:13:28 -0400

    update first

---
+++ src/main/java/yoo/byung_chun/abstract_class.java
@@ -0,0 +1,7 @@
+package yoo.byung_chun;
+
+/**
+ * Created by byung-chunyoo on 5/1/17.
+ */
+public class abstract_class {
+}
---
+++ src/main/java/yoo/byung_chun/rotate_array/RotateArray.java
@@ -0,0 +1,20 @@
+package yoo.byung_chun.rotate_array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by byung-chunyoo on 5/1/17.
+ */
+public class RotateArray extends ArrayList  {
+    public static void main (String[] args){
+
+        //subclasse List class
+        //creat an array
+        //method to rotate by k element
+        //
+
+
+
+    }
+
+}
