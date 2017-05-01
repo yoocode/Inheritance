de745e2df1632616a06b7f710007a5f1cddace9d
Byung-Chun Yoo <byung-chunyoo@zipcoder7.local>
2017-05-01 15:46:06 -0400

    updated RotateArray

--- src/main/java/yoo/byung_chun/rotate_array/RotateArray.java
+++ src/main/java/yoo/byung_chun/rotate_array/RotateArray.java
@@ -1,6 +1,9 @@
 package yoo.byung_chun.rotate_array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by byung-chunyoo on 5/1/17.
@@ -11,7 +14,22 @@ public class RotateArray extends ArrayList  {
         //subclasse List class
         //creat an array
         //method to rotate by k element
-        //
+        List myArray = new ArrayList();
+        for (int i = 1; i<7; i++) {
+            myArray.add(i);
+        }
+
+        Collections.rotate(myArray, 2);
+        System.out.println(Arrays.toString(myArray.toArray()));
+
+
+
+
+
+
+
+
+
 
 
 
